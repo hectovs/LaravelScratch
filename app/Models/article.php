d1b145ecb358ae787b46e2f21440dca60ea180fd
Hectovs <htjvans@gmail.com>
2020-12-14 08:42:44 +0000

    End of Episode 31
    Many to many relationships with linking tables. Articles and tags and a linking table.

--- app/Models/article.php
+++ app/Models/article.php
@@ -21,4 +21,9 @@ class Article extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
