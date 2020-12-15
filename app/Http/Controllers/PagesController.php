60e7354997490c481f0ca58325498f86cc279c34
Hectovs <htjvans@gmail.com>
2020-12-15 15:09:11 +0000

    End of episode 40
    Explanation of facades, understandable but not sure of applicability.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Example;
+use Illuminate\Auth\RequestGuard;
 use Illuminate\Http\Request;
+use Illuminate\support\Facades\Cache;
+use Illuminate\Filesystem\Filesystem;
 
 class PagesController extends Controller
 {
-    public function home(Example $example)
+    public function home()
     {
-        ddd(resolve('App\Models\Example'),resolve('App\Models\Example'));
+        
+        Cache::remember('foo', 60, fn()=> 'foobar');
+
+        return Cache::get('foo'); 
     }
 }
